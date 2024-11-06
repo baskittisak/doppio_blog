f06fb79c7839875e8315ec88ad5d3b497b066c46
Kittisak Srathong <basbubie.kittisak@gmail.com>
2024-11-07 00:18:23 +0700

    [UPDATE] empty content

--- client/src/components/blog/Blogs.tsx
+++ client/src/components/blog/Blogs.tsx
@@ -1,10 +1,16 @@
 import { memo } from "react";
+import "./style/blogs.css";
 import Menu from "./Menu";
+import Empty from "antd/lib/empty";
 
 function Blogs() {
   return (
     <div className="blogs-container">
       <Menu />
+      <Empty
+        className="empty-blog"
+        description="There's no blog post yet. Start by creating your first post and share your amazing ideas with the world!"
+      />
     </div>
   );
 }
